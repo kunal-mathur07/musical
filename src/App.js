49e5bcbd198ea037f4fd934146c23a3b03750cbb
Kunal Mathur <kunalmathur@Apples-MacBook-Pro.local>
2019-08-11 23:33:38 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
 import React from 'react';
 import logo from './logo.svg';
+import soundObject from './sound';
 import './App.css';
+import { returnStatement, thisExpression } from '@babel/types';
 
 function App() {
+  window.addEventListener('keydown',function(e){
+    document.querySelectorAll('.key').forEach((item)=>{
+      item.classList.remove('glow');
+    });
+    var key =  document.querySelector(`div[data-key='${e.keyCode}'`);  
+    var sound = document.querySelector(`audio[data-key='${e.keyCode}'`);
+    if(!key && !sound){
+      return;
+    }
+   
+    key.classList.add('glow');
+    sound.currentTime=0;
+    sound.play().then(function(){}).catch(function(err){});
+    
+  });
+
+  function handleClick(val){
+  
+   var sound = document.querySelector(`audio[data-key='${val}'`);
+    sound.currentTime=0;
+    sound.play().then(function(){}).catch(function(err){});
+  
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="key" data-key="65" onClick= {handleClick.bind(this,65)}>  
+       A
+       <span className="subtext">clap</span>
+     </div>
+     <div className="key" data-key="83" onClick= {handleClick.bind(this,83)}>
+       S
+       <span className="subtext">hithat</span>
+     </div>
+     <div className="key" data-key="68" onClick= {handleClick.bind(this,68)}>
+       D
+       <span className="subtext">kick</span>
+     </div>
+     <div className="key" data-key="70" onClick= {handleClick.bind(this,70)}>
+       F
+       <span className="subtext">openhat</span>
+     </div>
+     <div className="key" data-key="71" onClick= {handleClick.bind(this,71)}>
+       G
+       <span className="subtext">boom</span>
+     </div>
+     <div className="key" data-key="72" onClick= {handleClick.bind(this,72)}>
+       H
+       <span className="subtext">ride</span>
+     </div>
+     <div className="key" data-key="74" onClick= {handleClick.bind(this,74)}>
+       J
+       <span className="subtext">snare</span>
+     </div>
+     <div className="key" data-key="75" onClick= {handleClick.bind(this,75)}>
+       K
+       <span className="subtext">tom</span>
+     </div>
+     <div className="key" data-key="76" onClick= {handleClick.bind(this,76)}>
+       L
+       <span className="subtext">tink</span>
+     </div>
+    
+
+     <audio data-key="65" src={soundObject.clap}/>
+     <audio data-key="83" src={soundObject.hihat}/>
+     <audio data-key="68" src={soundObject.kick}/>
+     <audio data-key="70" src={soundObject.openhat}/>
+     <audio data-key="71" src={soundObject.boom}/>
+     <audio data-key="72" src={soundObject.ride}/>
+     <audio data-key="74" src={soundObject.snare}/>
+     <audio data-key="75" src={soundObject.tom}/>
+     <audio data-key="76" src={soundObject.tink}/>
     </div>
   );
 }
---
+++ src/sound.js
@@ -0,0 +1,28 @@
+import clap from './sounds/clap.wav';
+import boom from './sounds/boom.wav';
+import hihat from './sounds/hihat.wav';
+import kick from './sounds/kick.wav';
+import openhat from './sounds/openhat.wav';
+import ride from './sounds/ride.wav';
+import snare from './sounds/snare.wav';
+import tink from './sounds/tink.wav';
+import tom from './sounds/tom.wav';
+
+const soundObject={
+    clap:clap,
+    boom:boom,
+    hihat:hihat,
+    kick:kick,
+    openhat:openhat,
+    ride:ride,
+    snare:snare,
+    tink:tink,
+    tom:tom
+
+}
+
+export default soundObject; 
+
+
+
+
